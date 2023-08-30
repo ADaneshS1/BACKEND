771e7a0e4339789dfb10aaebef321e5319520587
Ahmad Danesh Sudiono <ahmaddaneshsudiono1.ppqita@gmail.com>
2023-08-30 10:29:06 +0700

    Buat Server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+
+// Example
+/*
+    - req = singkatan dari request. Yang isinya yang dikirimkan oleh client. Contoh seperti body, parameter, query
+    - res = singkatan dari response. Yang isinya kita kirim ke client. Contoh seperti data, json, html, dan codeHTTP (default:200)
+*/
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+// Untuk running
+app.listen(3000, () => {
+    console.log("Halo cuyy, Server sudah jalan di localhost:3000")
+})
+
+// Untuk menjalankan tulis node app.js di terminal
