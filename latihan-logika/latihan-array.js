8f56196a9d143bb3cf401b3fa0cbd9454faa8949
Ahmad Danesh Sudiono <ahmaddaneshsudiono1.ppqita@gmail.com>
2023-09-01 13:35:29 +0700

    T-01092023

--- latihan-logika/latihan-array.js
+++ latihan-logika/latihan-array.js
@@ -1,37 +1,84 @@
-const getLargestNumber = (bank, data) => {
-    bank % 2;
-    return bank;
-};
-
-const filterEvenNumber = (bank) => {
-    const index = bank.filter((value) => value % 2)
-    return index;
-};
-
-const sumArray = (bank) => {
-    const index = bank.reduce((value, val) => value + val)
-    return index;
-};
-
-const findDuplicates = (bank) => {
-    const index = bank.filter((item, index) => bank.indexOf(item) !== index)
-    const dupli = index(bank)
-    return dupli;
-};
-
-const sortArray = (bank) => {
-    const index = bank.sort((value, val) => value - val)
-    return index;
-};
-
-const calculateAverage = (bank) => {
-    let sum = 0
-    for (let number of bank) {
-        sum += number;
+function getLargestNumber(arr) {
+    if (arr.length === 0) {
+      throw new Error("Array is empty");
     }
-    average = sum / bank.length;
-    return average;
-};
-
-
-module.exports = { getLargestNumber, filterEvenNumber, sumArray, sortArray, calculateAverage, findDuplicates };
+  
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
+    }
+  
+    return largest;
+  }
+  
+  // Fungsi untuk menyaring angka genap dalam array
+  function filterEvenNumber(arr) {
+    return arr.filter((num) => num % 2 === 0);
+  }
+  
+  // Fungsi untuk menjumlahkan semua angka dalam array
+  function sumArrayNumber(arr) {
+    return arr.reduce((total, num) => total + num, 0);
+  }
+  
+  // Fungsi untuk mencari angka duplikat dalam array
+  function findDuplicates(arr) {
+    const seen = {};
+    const duplicates = [];
+  
+    for (const num of arr) {
+      if (seen[num]) {
+        duplicates.push(num);
+      } else {
+        seen[num] = true;
+      }
+    }
+  
+    return duplicates;
+  }
+  
+  // Fungsi untuk mengurutkan array secara ascending
+  function sortArray(arr) {
+    return arr.slice().sort((a, b) => a - b);
+  }
+  
+  // Fungsi untuk menggabungkan dua array menjadi satu
+  function concatenateArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+  }
+  
+  // Fungsi untuk menghitung rata-rata dari angka dalam array
+  function calculateAverage(arr) {
+    const sum = arr.reduce((total, num) => total + num, 0);
+    return sum / arr.length;
+  }
+  
+  // Fungsi untuk memeriksa apakah semua angka dalam array positif
+  function allPositive(arr) {
+    return arr.every((num) => num > 0);
+  }
+  
+  // Fungsi untuk menghapus nilai-nilai duplikat dari array
+  const removeDuplicates = (arr) => {
+    return [...new Set(arr)];
+  }
+  
+  // Fungsi untuk mencari indeks pertama dari angka tertentu dalam array
+  const findIndexOf = (arr, target) => {
+    return arr.indexOf(target);
+  }
+  
+  module.exports = {
+    getLargestNumber,
+    filterEvenNumber,
+    sumArrayNumber,
+    sortArray,
+    concatenateArrays,
+    findDuplicates,
+    calculateAverage,
+    allPositive,
+    removeDuplicates,
+    findIndexOf,
+  };
--- latihan-logika/latihan-array.test.js
+++ latihan-logika/latihan-array.test.js
@@ -1,45 +1,45 @@
-const {getLargestNumber, filterEvenNumber, sumArray, findDuplicates, sortArray,
-    concatenateArrays, calculateAverage, allPositive, removeDuplicates, findIndexOf
-} = require("./latihan-array")
-
-describe('Array Operations', () => {
-    test('1. Menemukan angka terbesar dalam array', () => {
-        expect(getLargestNumber([5,8,2,10,3])).toBe(10);
+const {
+    getLargestNumber,
+    filterEvenNumber,
+    sumArrayNumber,
+    findDuplicates,
+    sortArray,
+    concatenateArrays,
+    calculateAverage,
+    allPositive,
+    removeDuplicates,
+    findIndexOf,
+  } = require("./latihan-array");
+  
+  describe("Array Operations", () => {
+    test("1. Menemukan angka terbesar dalam array", () => {
+      expect(getLargestNumber([5, 8, 2, 10, 3])).toBe(10);
     });
-
-    test('2. Filter angka genap dari array', () => {
-        expect(filterEvenNumber([1,2,3,4,5,6])).toEqual([2,4,6]);
+    test("2. Filter angka genap di array", () => {
+      expect(filterEvenNumber([1, 2, 3, 4, 5, 6])).toEqual([2, 4, 6]);
     });
-
-    test('3. Menjumlahkan semua angka dalam array', () => {
-        expect(sumArray([1,2,3,4,5])).toBe(15);
+    test("3. Menjumlahkan semua angka dalam array", () => {
+      expect(sumArrayNumber([1, 2, 3, 4, 5])).toBe(15);
     });
-
-    test('4. Mencari angka duplikat dalam array', () => {
-        expect(findDuplicates([1,2,3,4,2,5,5])).toEqual([2,5]);
+    test("4. Mencari angka duplikat dalam array", () => {
+      expect(findDuplicates([1, 2, 3, 2, 4, 5, 5])).toEqual([2, 5]);
     });
-
-    test('5. Mengurutkan array secara ascending', () => {
-        expect(sortArray([3,1,4,2,5])).toEqual([1,2,3,4,5]);
+    test("5. Mengurutkan array secara ascending", () => {
+      expect(sortArray([3, 1, 4, 2, 5])).toEqual([1, 2, 3, 4, 5]);
     });
-
-    test('6. Menggabungkan dua array menjadi satu', () => {
-        expect(concatenateArrays([1,2,3], [4,5,6])).toEqual([1,2,3,4,5,6]);
+    test("6. Menggabungkan dua array menjadi satu", () => {
+      expect(concatenateArrays([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
     });
-
-    test('7. Menghitung rata-rata dari angka dalam array', () => {
-        expect(calculateAverage([10,20,30,40,50])).toBe(30);
+    test("7. Menghitung rata-rata dari angka dalam array", () => {
+      expect(calculateAverage([10, 20, 30, 40, 50])).toBe(30);
     });
-
-    test('8. Mengecek apakah semua angka dalam array positif', () => {
-        expect(allPositive([1,2,3,-4,5])).toBe(false);
+    test("8. Mengecek apakah semua angka dalam array positif", () => {
+      expect(allPositive([1, 2, 3, -4, 5])).toBe(false);
     });
-
-    test('9. Menghapus nilai-nilai duplikat dari array', () => {
-        expect(removeDuplicates([1,2,3,2,4,5,5])).toEqual([1,2,3,4,5]);
+    test("9. Menghapus nilai-nilai duplikat dari array", () => {
+      expect(removeDuplicates([1, 2, 3, 2, 4, 5, 5])).toEqual([1, 2, 3, 4, 5]);
     });
-
-    test('10. Mencari index pertama dari angka tertentu dalam array', () => {
-        expect(findIndexOf([10,20,30,40,50])).toBe(2);
+    test("10. Mencari indeks pertama dari angka tertentu dalam array", () => {
+      expect(findIndexOf([10, 20, 30, 40, 50], 30)).toBe(2);
     });
-});
+  });
