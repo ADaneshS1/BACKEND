777fe3508be3f586e599f69e472a69718acc090b
Ahmad Danesh Sudiono <ahmaddaneshsudiono1.ppqita@gmail.com>
2023-09-01 11:04:29 +0700

    1923

--- latihan-logika/latihan-array.js
+++ latihan-logika/latihan-array.js
@@ -1,30 +1,37 @@
-const createData = (bank, data) => {
-    bank.push(data);
-
+const getLargestNumber = (bank, data) => {
+    bank % 2;
     return bank;
 };
 
-const addObj = (bank, data) => {
-    bank.push(data);
-
-    return bank;
+const filterEvenNumber = (bank) => {
+    const index = bank.filter((value) => value % 2)
+    return index;
 };
 
-const findData = (bank, id) => {
-    return bank.find((value) => value.id === id);
-};
-const updateData = (bank, id, value) => {
-    const index = bank.findIndex((value) => value.id === id);
-
-    id[index] = value;
-
-    return bank;
-};
-const deleteData = (bank, id) => {
-    const index = bank.findIndex((value) => value.id === id);
-
-    bank.splice(index, 1);
-    return bank;
+const sumArray = (bank) => {
+    const index = bank.reduce((value, val) => value + val)
+    return index;
 };
 
-module.exports = { createData, addObj, findData, updateData, deleteData };
+const findDuplicates = (bank) => {
+    const index = bank.filter((item, index) => bank.indexOf(item) !== index)
+    const dupli = index(bank)
+    return dupli;
+};
+
+const sortArray = (bank) => {
+    const index = bank.sort((value, val) => value - val)
+    return index;
+};
+
+const calculateAverage = (bank) => {
+    let sum = 0
+    for (let number of bank) {
+        sum += number;
+    }
+    average = sum / bank.length;
+    return average;
+};
+
+
+module.exports = { getLargestNumber, filterEvenNumber, sumArray, sortArray, calculateAverage, findDuplicates };
